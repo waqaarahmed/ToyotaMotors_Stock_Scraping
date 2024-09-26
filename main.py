5ff754f99fb1ea7c40bdca4b145452c0ee679625
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-09-26 23:38:08 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import yfinance as yf
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import tkinter as tk
+from tkinter import scrolledtext
