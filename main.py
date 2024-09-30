b83e802f5f41fee723b3744368ec0036b77aec0f
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-09-30 22:05:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -81,3 +81,32 @@ def show_data_in_window(title, data):
     
     # Start the window loop
     window.mainloop()
+
+# Toyota Data
+toyota_data = yf.Ticker('TM').history(period='max').reset_index()
+toyota_revenue = extract_revenue_data('https://www.macrotrends.net/stocks/charts/TM/toyota/revenue', 'Toyota Quarterly Revenue')
+
+# Clean Toyota revenue data
+toyota_revenue = toyota_revenue[toyota_revenue['Revenue'] != '']
+
+# Show Toyota Data in a new window
+show_data_in_window("Toyota Motors Stock Data", toyota_data)
+show_data_in_window("Toyota Motors Revenue Data", toyota_revenue)
+
+# Plot Toyota Data
+plot_graph(toyota_data, toyota_revenue, 'Toyota Motors Historical Share Price & Revenue')
+
+# Honda Data
+honda_data = yf.Ticker('HMC').history(period='max').reset_index()
+honda_revenue = extract_revenue_data('https://www.macrotrends.net/stocks/charts/HMC/honda/revenue', 'Honda Quarterly Revenue')
+
+# Clean Honda revenue data
+honda_revenue = honda_revenue[honda_revenue['Revenue'] != '']
+
+# Show Honda Data in a new window
+show_data_in_window("Honda Stock Data", honda_data)
+show_data_in_window("Honda Revenue Data", honda_revenue)
+
+# Plot Honda Data
+plot_graph(honda_data, honda_revenue, 'Honda Historical Share Price & Revenue')
+
